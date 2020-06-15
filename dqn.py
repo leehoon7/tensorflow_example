d3bdbc2cc77b6f3d7356680171fdbee9a33702ce
unknown <leehoon@kaist.ac.kr>
2020-06-15 22:36:15 +0900

    cnn network finished

--- dqn.py
+++ dqn.py
@@ -1,14 +1,14 @@
 import gym
 import tensorflow as tf
 import numpy as np
-from policy_network import Policy
 import matplotlib.pyplot as plt
 import time
+import random
 
 
 def prepro(I, render = False):
 
-    """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
+    # pre-process 210 x 160 x 3 frame into 6400 (80x80) 1D float vector
 
     I = I[35:195]  # crop
     I = I[::2, ::2, 0]  # down-sample by factor of 2
@@ -42,51 +42,104 @@ def check_img_diff():
 
 def main():
     env = gym.make("Pong-v0")
+
     obs = env.reset()
+    epsilon = 1.0
+
+    # pre-process observation
     obs = prepro(obs)
     obs = np.reshape(obs, [1, 6400])
 
+    # set placeholder
     X = tf.placeholder(tf.float32, [None, 6400])
     X_img = tf.reshape(X, [-1, 80, 80, 1])
-    # 32 filters : 3 * 3 * 1
+
+    # 1st layer : 4 filters, 8 * 8 kernel, 1 input channel
     W1 = tf.Variable(tf.random_normal([8, 8, 1, 4], stddev=0.1))
     b1 = tf.Variable(tf.zeros([4]))
     L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
     L1 = tf.nn.relu(L1 + b1)
     L1 = tf.nn.max_pool(L1, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding='SAME')
 
+    # 2nd layer : 8 filters, 4 * 4 kernel, 4 input channels
     W2 = tf.Variable(tf.random_normal([4, 4, 4, 8], stddev=0.1))
     b2 = tf.Variable(tf.zeros([8]))
     L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
     L2 = tf.nn.relu(L2 + b2)
     L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
+    # 3rd layer : 16 filters, 2 * 2 kernel, 8 input channels
     W3 = tf.Variable(tf.random_normal([2, 2, 8, 16], stddev=0.1))
     b3 = tf.Variable(tf.zeros([16]))
     L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
     L3 = tf.nn.relu(L3 + b3)
     L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # reshape : 2D -> 1D
     L3 = tf.reshape(L3, [-1, 5 * 5 * 16])
 
+    # 4th layer : neural network, 400 -> 20
     W4 = tf.Variable(tf.random_normal([5 * 5 * 16, 20], stddev=0.1))
     b4 = tf.Variable(tf.zeros([20]))
     L4 = tf.matmul(L3, W4)
     L4 = tf.nn.relu(L4 + b4)
 
-    #W5 = tf.Variable(tf.)
+    # 5th layer : linear mapping, 20 -> 6
+    W5 = tf.Variable(tf.random_normal([20, 6], stddev=0.1))
+    b5 = tf.Variable(tf.zeros([6]))
+    L5 = tf.matmul(L4, W5)
+    L5 = L5 + b5
 
-    loss = L4[0][0] - 1
+    # policy to use
+    q_value = L5[0]
+
+    # loss to minimize
+    loss = L5[0][0] - 1
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
+    # make a session
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
-    for i in range(1):
-        l1, l2, l3, l4, b, c = sess.run([L1, L2, L3, L4, loss, optimizer], feed_dict={X: obs})
 
-        print(l1.shape)
-        print(l2.shape)
-        print(l3.shape)
-        print(l4.shape)
+    while True:
+        # render game screen
+        env.render()
+
+        action = None
+
+        # 'random action' or get 'q-value from network'
+        if random.random() < epsilon:
+            action = random.randint(0, 5)
+        else:
+            policy = sess.run([q_value], feed_dict={X: obs})
+            action = np.argmax(np.array(policy))
+
+
+
+        # do action and get new information
+        obs, reward, done, info = env.step(action)
+
+        print(action, reward, done, info)
+
+        obs = prepro(obs)
+        obs = np.reshape(obs, [1, 6400])
+        time.sleep(0.1)
+
+        '''
+
+        policy, loss, _ = sess.run([dqn_policy, loss, optimizer], feed_dict={X: obs})
+
+
+        obs, rew, done, info = env.step(5)
+
+        obs = prepro(obs)
+        obs = np.reshape(obs, [1, 6400])
+        time.sleep(0.1)
+
+        print(policy, loss)
+        '''
+
+    env.close()
 
 
 if __name__ == '__main__':
