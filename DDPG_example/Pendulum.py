f5d27210cb6afd847cc82e8f5df44e25c7c9ff82
unknown <leehoon@kaist.ac.kr>
2020-06-19 21:09:51 +0900

    actor, critic network

--- DDPG_example/Pendulum.py
+++ DDPG_example/Pendulum.py
@@ -17,22 +17,33 @@ def main():
     act_learning_rate = 0.001
     print(state_dim, action_dim, action_bound)
 
-    X = tf.placeholder(tf.float32, [None, state_dim])
+    S = tf.placeholder(tf.float32, [None, state_dim])
+    A = tf.placeholder(tf.float32, [None, action_dim])
 
     ''' actor network '''
     W1 = tf.Variable(tf.random_normal([state_dim, 64], stddev=0.1))
     b1 = tf.Variable(tf.zeros([64]))
-    L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+    L1 = tf.nn.relu(tf.matmul(S, W1) + b1)
 
     W2 = tf.Variable(tf.random_normal([64, 1], stddev=0.1))
     actor_output = tf.nn.tanh(tf.matmul(L1, W2)) * action_bound
 
+    ''' critic network '''
+    C_W1 = tf.Variable(tf.random_normal([state_dim + action_dim, 64], stddev=0.1))
+    C_b1 = tf.Variable(tf.zeros([64]))
+    C_L1 = tf.nn.relu(tf.matmul(tf.concat([S, A], 1), C_W1) + C_b1)
+
+    C_W2 = tf.Variable(tf.random_normal([64, 1], stddev=0.1))
+    critic_output = tf.matmul(C_L1, C_W2)
+
+
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
-    action = sess.run(actor_output, feed_dict={X: obs})
+    action = sess.run(actor_output, feed_dict={S: obs})
+    critic_val = sess.run(critic_output, feed_dict={S: obs, A: action})
 
-    print(action)
+    print(action, critic_val)
 
 
 if __name__ == '__main__':
