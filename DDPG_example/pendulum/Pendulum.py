6d0ac6e9917ab5d83c5e041a96048174ac07d225
unknown <leehoon@kaist.ac.kr>
2020-06-20 16:17:56 +0900

    comment

--- DDPG_example/pendulum/Pendulum.py
+++ DDPG_example/pendulum/Pendulum.py
@@ -71,7 +71,6 @@ def main():
     A_optimizer = tf.train.AdamOptimizer(learning_rate=actor_learning_rate).minimize(A_loss, var_list=[A_W1, A_b1, A_W2])
 
     ''' actor target network '''
-
     AT_W1 = tf.Variable(tf.random_normal([state_dim, 64], stddev=0.1))
     AT_b1 = tf.Variable(tf.zeros([64]))
     AT_L1 = tf.nn.relu(tf.matmul(S, AT_W1) + AT_b1)
@@ -101,15 +100,16 @@ def main():
     t_up5 = CT_b1.assign(tau * CT_b1 + (1 - tau) * C_b1)
     t_up6 = CT_W2.assign(tau * CT_W2 + (1 - tau) * C_W2)
 
-    ''' check actor & critic'''
+    ''' check actor & critic '''
     action = sess.run(actor_output, feed_dict={S: obs})
     critic_val = sess.run(critic_output, feed_dict={S: obs, A: action})
     target_action = sess.run(target_actor_output, feed_dict={S: obs})
     target_critic_val = sess.run(target_critic_output, feed_dict={S: obs, A: action})
 
-    #print(action, critic_val)
-    #print(target_action, target_critic_val)
+    print(action, critic_val)
+    print(target_action, target_critic_val)
 
+    ''' episode data '''
     A_loss_epi = []
     C_loss_epi = []
     reward_epi = []
@@ -124,21 +124,25 @@ def main():
 
         while not done:
 
-            # action with gaussian noise for exploration
+            ''' action with gaussian noise for exploration '''
+            ''' action should be in [-2, 2] --> clip action value '''
             action = sess.run(actor_output, feed_dict={S: obs})
             action = action[0] + noise_epsilon * np.random.normal(0, 0.5, 1)
             action = np.clip(action, -action_bound, action_bound)
 
-            # save a state before do action
+            ''' save a state before do action '''
             bef_obs = obs
 
+            ''' do action and get new state, reward, .. '''
             obs, reward, done, _ = env.step(action)
             obs = obs.reshape(-1, 3)
 
+            ''' save transition to replay memory '''
             reward_list.append(reward)
             transition = [bef_obs[0], action, reward, obs[0], done]
             replay_memory.append(transition)
 
+            ''' training process '''
             if len(replay_memory) >= batch_size:
                 train_data = random.sample(replay_memory, batch_size)
 
@@ -148,6 +152,7 @@ def main():
                 t_aft_state = np.stack([data[3] for data in train_data])
                 t_terminal = np.array([data[4] for data in train_data])
 
+                ''' compute target : 12 line in pseudocode '''
                 action_next = sess.run(target_actor_output, feed_dict={S: t_aft_state})
                 q_val = sess.run(target_critic_output, feed_dict={S: t_aft_state, A: action_next})
                 q_val = q_val.squeeze()
@@ -157,17 +162,22 @@ def main():
                 batch_target = t_reward + gamma * q_val * t_terminal
                 batch_target = batch_target.reshape(-1, 1)
 
+                ''' update Q-function : 13 line in pseudocode '''
                 C_loss_val, _ = sess.run([C_loss, C_optimizer], feed_dict={S: t_bef_state, A: t_action, T: batch_target})
                 C_loss_list.append(C_loss_val)
 
+                ''' update policy : 14 line in pseudocode '''
                 A_loss_val, _ = sess.run([A_loss, A_optimizer], feed_dict={S: t_bef_state})
                 A_loss_list.append(A_loss_val)
 
+                ''' update target networks with tau : 15 line in pseudocode '''
                 sess.run([t_up1, t_up2, t_up3, t_up4, t_up5, t_up6])
 
+            ''' render when episode over 1000 '''
             if cur_episode > 1000:
                 env.render()
 
+        ''' print episode information : loss, reward, ... etc. '''
         if len(replay_memory) > batch_size:
 
             C_loss_epi.append(sum(C_loss_list) / len(C_loss_list))
@@ -188,9 +198,11 @@ def main():
         obs = env.reset()
         obs = obs.reshape(-1, 3)
 
+        ''' decay epsilon for less exploration '''
         if noise_epsilon > noise_epsilon_min:
             noise_epsilon -= noise_epsilon_decay
 
+    ''' final loss, reward data for plotting '''
     print(A_loss_epi)
     print(C_loss_epi)
     print(reward_epi)
